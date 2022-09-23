ca91ac831316ac94b27454cc085c5b467788829a
Matus <vmatus106711@gmail.com>
2022-09-23 14:23:13 +0300

    Fibonacci number

--- prog.py
+++ prog.py
@@ -1,2 +1,5 @@
-print("hello world")
-print("git lab")
+import math
+num = ((((1 + math.sqrt(5))/2)**1)-(((1 - math.sqrt(5))/2)**1))/math.sqrt(5)
+for counter in range(10):
+    num = ((((1 + math.sqrt(5))/2)**counter)-(((1 - math.sqrt(5))/2)**counter))/math.sqrt(5)
+    print(round(num))
